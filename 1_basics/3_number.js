8271563cd63aa8f2638a2c5bf8977282070633fa
Karan Nayal <karannayal1234567@gmail.com>
2026-07-21 16:51:20 +0530

    3_numbers

---
+++ 1_basics/3_number.js
@@ -0,0 +1,23 @@
+const score = 100;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString());
+
+//console.log(balance.toFixed(2)); // helps in fixed digit like in a commerce website the customers only want to see upto 2 value..
+const hundreds = 10000000;
+
+const othernumber = 123.576;
+//console.log(othernumber.toPrecision(3));
+
+//console.log(hundreds.toLocaleString('en-IN')); // helps in formatting the number with commas and dots based on the locale.
+
+
+//-------------maths---------------
+
+//console.log(Math);
+console.log(Math.random());
+
+
